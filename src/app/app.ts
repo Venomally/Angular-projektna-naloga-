1c61f3d597cbd1a6807a79d1fb8f58d9c26f8f15
Amer Čengić <87905215+Venomally@users.noreply.github.com>
2026-01-08 12:27:49 +0100

    adding basic compopnents like Nabbar, footer and Books elements

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { Books } from './components/books/books';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Footer, Books],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('AngularProjekt');
+  title = signal('AngularProjekt');
 }
